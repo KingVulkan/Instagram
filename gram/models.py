9bd47a1f7df78faf61aa44eff33fa494edb61cf8
KingVulkan <manowfelow@gmail.com>
2019-03-09 22:45:21 +0300

    creates a comment class in models.py

--- gram/models.py
+++ gram/models.py
@@ -77,3 +77,25 @@ class Image(models.Model):
         function that gets all posts of the people that the current user follower
         '''
         timeline_posts = Image.objects.filter()
+
+class Comment(models.Model):
+    '''
+    class that defines the structure of an comment on image
+    '''
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE, null= True)
+    image_id = models.ForeignKey(Image,on_delete=models.CASCADE, null= True)
+    comment= models.TextField(blank=True)
+
+    def __str__(self):
+        return self.comment
+
+    def save_comment(self):
+        '''
+        method that save a comment on an image
+        '''
+        self.sa
+    def delete_comment(self):
+        '''
+        methods that deletes a comment on an image
+        '''
+        self.delete()
