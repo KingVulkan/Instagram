20e4a6678761b6da33862ae48f26c41fbc47ae90
KingVulkan <manowfelow@gmail.com>
2019-03-10 15:44:18 +0300

    adds functions for updating profile ,comment,profile and viewing profiles

--- gram/views.py
+++ gram/views.py
@@ -86,3 +86,99 @@ def post(request):
     else:
         form = ImageForm() 
     return render(request, 'all-grams/post.html',{"form" : form}) 
+
+@login_required(login_url='/accounts/login/')
+def comment(request, image_id):
+    comments = Comment.objects.filter(image_id=image_id)
+    current_image = Image.objects.get(id=image_id)
+    current_user = request.user
+
+    if request.method == 'POST':
+
+        form = CommentForm(request.POST)
+        logger_in = request.user
+        
+
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.user_id= current_user
+            comment.image_id = current_image
+            current_image.comments_number+=1
+            current_image.save_image()
+            comment.save()
+
+            return redirect(timeline)
+    else:
+        form = CommentForm()
+    return render(request,'all-grams/comment.html',{"form":form,"comments":comments})  
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user = request.user 
+    title = 'Update Profile'
+    try:
+        requested_profile = Profile.objects.get(user_id = current_user.id)
+        if request.method == 'POST':
+            form = ProfileUpdateForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                requested_profile.profile_photo = form.cleaned_data['profile_photo']
+                requested_profile.bio = form.cleaned_data['bio']
+                requested_profile.username = form.cleaned_data['username']
+                requested_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUpdateForm()
+    except:
+        if request.method == 'POST':
+            form = ProfileUpdateForm(request.POST,request.FILES)
+
+            if form.is_valid():
+                new_profile = Profile(profile_photo= form.cleaned_data['profile_photo'],bio = form.cleaned_data['bio'],username = form.cleaned_data['username'],user = current_user)
+                new_profile.save_profile()
+                return redirect( profile )
+        else:
+            form = ProfileUpdateForm()
+
+
+    return render(request,'profile/update_profile.html',{"title":title,"current_user":current_user,"form":form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    title = 'Profile'
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(user_id = current_user)
+        following = Follow.objects.filter(follower = current_user)
+        followers = Follow.objects.filter(user = profile) 
+    except:
+        profile = Profile.objects.get(username = 'default_user')
+        following = Follow.objects.filter(follower = current_user)
+        followers = Follow.objects.filter(user = profile)
+
+    return render(request, 'profile/profile.html',{"profile":profile,"current_user":current_user,"following":following,"followers":followers})
+
+@login_required(login_url='/accounts/login/')
+def more(request,image_id):
+    image = Image.objects.get(id = image_id)
+
+    current_user = request.user
+    update_image = Image.objects.get(id= image_id)
+
+    if request.method == 'POST':
+        form = UpdateImageCaption(request.POST)
+        if form.is_valid():
+            new_caption = form.cleaned_data['image_caption']
+            update_image.image_caption = new_caption
+            update_image.save_image() 
+
+            return redirect( more ,image_id)
+    else:
+        form = UpdateImageCaption()
+
+    return render(request,'all-grams/more.html',{"image":image, "form":form}) 
+
+@login_required(login_url='/accounts/login/')
+def view_profiles(request):
+    all_profiles = Profile.objects.all()
+    return render(request,'profile/all.html',{"all_profiles":all_profiles}) 
